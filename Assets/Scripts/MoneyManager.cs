8008b26bf94b022c7c1652ed739aac0ae93470a2
mimugon <49438383@est.ort.edu.ar>
2025-06-05 12:14:01 -0300

    ultimas clases

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoneyManager : MonoBehaviour
+{
+    public float money;
+    public UIManager uiManager;
+
+    void Start()
+    {
+        uiManager.UpdateMoneyTxt(money.ToString());
+    }
+
+    public void UpdateMoney(float moneyToAdd)
+    {
+        if (money + moneyToAdd < 0)
+        {
+            Debug.Log("No te alcanza");
+            return;
+        }
+        money += moneyToAdd;
+    }
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UIManager : MonoBehaviour
+{
+    public TextMeshProUGUI TXTMoney;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void UpdateMoneyTxt(string moneyAmount)
+     {
+        TXTMoney.text = moneyAmount;
+
+     }
+}
